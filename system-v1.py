18b87183b11a03ff135b9777a34f67c88d8d9c8b
Codershell911 <147912736+Codershell911@users.noreply.github.com>
2023-12-16 21:52:08 +0100

    system-v1.py

--- system-v1.py
+++ system-v1.py
@@ -1,2 +1,15 @@
+# system-v1.py
+
 def update_dapp():
-  # COMING SOON
+    try:
+        # Assuming 'dapp-exten.py' is in the same directory
+        with open('dapp-exten.py', 'w') as file:
+            file.write("# Updated content for dapp-exten.py\n")
+        return "Dapp-sys updated to version 1. Updated dapp-exten.py."
+    except Exception as e:
+        return f"Error updating dapp-sys: {e}"
+
+# Add any additional functions or code as needed
+
+if __name__ == "__main__":
+    print("This is system-v1.py")
